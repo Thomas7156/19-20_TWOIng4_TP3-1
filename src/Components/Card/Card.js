2f89254bdeb7e78d22c38e68a38aa0271da217d0
Thomas <thomas.lemercier@edu.ece.fr>
2019-11-10 17:56:03 +0100

    Ajout card bootstrap + problème img

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Card.css'
 import { connect } from 'react-redux'
+import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol } from 'mdbreact';
 
 class Card extends React.Component {
   constructor(props) {
@@ -28,6 +29,23 @@ class Card extends React.Component {
         <div>
           {this.props.Current[4]}
         </div>
+        <div>
+          {this.props.Current[5]}
+        </div>
+
+        <MDBCol>
+          <MDBCard style={{ width: "22rem" }}>
+            <MDBCardImage className="img-fluid" src={(this.props.Current[4])} alt={this.props.Current[4]} waves />
+            <MDBCardBody>
+              <MDBCardTitle>Card title</MDBCardTitle>
+              <MDBCardText>
+                Some quick example text to build on the card title and make
+                up the bulk of the card&apos;s content.
+              </MDBCardText>
+              <MDBBtn href="#">MDBBtn</MDBBtn>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App/App.js'
 import { Provider } from 'react-redux'
 import Store from './Store/ConfigureStore.js'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
 
 ReactDOM.render(
   <Provider store={ Store }>
     <App/>
-  </Provider>, 
+  </Provider>,
   document.getElementById('root')
 );
